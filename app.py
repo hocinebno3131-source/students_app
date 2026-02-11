0490474980091e67588428baf312e88e190b2986
hocinebno3131-source <hocinebno3131@gmail.com>
2026-02-11 15:32:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
-You reached the start of the range
-Feb 11, 2026, 3:12 PM
 Starting Container
   File "/app/app.py", line 5
     <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
 SyntaxError: invalid character '—' (U+2014)
-SyntaxError: invalid character '—' (U+2014)
-  File "/app/app.py", line 5
-    <title>لوحة الأدمن — عرض الطلبة</title>
-                       ^
-  File "/app/app.py", line 5
-    <title>لوحة الأدمن — عرض الطلبة</title>
-                       ^
-SyntaxError: invalid character '—' (U+2014)
   File "/app/app.py", line 5
     <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
@@ -44,17 +34,13 @@ SyntaxError: invalid character '—' (U+2014)
   File "/app/app.py", line 5
     <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
-  File "/app/app.py", line 5
-    <title>لوحة الأدمن — عرض الطلبة</title>
 SyntaxError: invalid character '—' (U+2014)
-                       ^
-SyntaxError: invalid character '—' (U+2014)
-  File "/app/app.py", line 5
   File "/app/app.py", line 5
     <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
-    <title>لوحة الأدمن — عرض الطلبة</title>
 SyntaxError: invalid character '—' (U+2014)
+  File "/app/app.py", line 5
+    <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
 SyntaxError: invalid character '—' (U+2014)
   File "/app/app.py", line 5
@@ -69,6 +55,13 @@ SyntaxError: invalid character '—' (U+2014)
     <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
 SyntaxError: invalid character '—' (U+2014)
+  File "/app/app.py", line 5
+    <title>لوحة الأدمن — عرض الطلبة</title>
+                       ^
+SyntaxError: invalid character '—' (U+2014)
+  File "/app/app.py", line 5
+    <title>لوحة الأدمن — عرض الطلبة</title>
+                       ^
 SyntaxError: invalid character '—' (U+2014)
   File "/app/app.py", line 5
     <title>لوحة الأدمن — عرض الطلبة</title>
@@ -77,6 +70,15 @@ SyntaxError: invalid character '—' (U+2014)
   File "/app/app.py", line 5
     <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
+SyntaxError: invalid character '—' (U+2014)
+  File "/app/app.py", line 5
+    <title>لوحة الأدمن — عرض الطلبة</title>
+                       ^
+SyntaxError: invalid character '—' (U+2014)
+  File "/app/app.py", line 5
+    <title>لوحة الأدمن — عرض الطلبة</title>
+                       ^
+SyntaxError: invalid character '—' (U+2014)
   File "/app/app.py", line 5
     <title>لوحة الأدمن — عرض الطلبة</title>
                        ^
