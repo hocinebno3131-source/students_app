f7babe5dff7229918438b8068f5847a53bac363b
hocinebno3131-source <hocinebno3131@gmail.com>
2026-02-18 10:18:38 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if(urlParams.get("duplicate") === "1") {
+        document.getElementById("duplicateModal").style.display = "flex";
+    }
+});
 
+function closeDuplicateModal() {
+    document.getElementById("duplicateModal").style.display = "none";
+}
